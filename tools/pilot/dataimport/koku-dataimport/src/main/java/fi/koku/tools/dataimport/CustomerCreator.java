b379e85b9a4e7712f51d3b3a827dca0e00230fcf
Antti Hanhineva <antti.hanhineva@ixonos.com>
2011-11-17 14:27:14 +0000

    New version

--- tools/pilot/dataimport/koku-dataimport/src/main/java/fi/koku/tools/dataimport/CustomerCreator.java
+++ tools/pilot/dataimport/koku-dataimport/src/main/java/fi/koku/tools/dataimport/CustomerCreator.java
@@ -7,22 +7,9 @@ public class CustomerCreator {
   public void createCustomers(CSVReader reader, WSCaller caller) throws Exception {
     String[] nextLine;
     while ((nextLine = reader.readNext()) != null) {
-      System.out.println(nextLine[0]);
-      System.out.println(nextLine[1]);
-      System.out.println(nextLine[2]);
-      System.out.println(nextLine[3]);
-      System.out.println(nextLine[4]);
-      System.out.println(nextLine[5]);
-      System.out.println(nextLine[6]);
-      System.out.println(nextLine[7]);
-      System.out.println(nextLine[8]);
-      System.out.println(nextLine[9]);
-      System.out.println(nextLine[10]);
-      System.out.println(nextLine[11]);
-      System.out.println(nextLine[12]);
-      
-      //caller.addCustomers(reader)
-      caller.addCustomer();
+
+//      caller.addCustomer();
+//      caller.addCustomer();
     }
   }
 }
