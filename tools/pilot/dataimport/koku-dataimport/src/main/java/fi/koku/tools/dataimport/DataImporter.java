8be3402724da28be2bce1e93e6e0e428d63111b8
Antti Hanhineva <antti.hanhineva@ixonos.com>
2011-11-18 13:47:01 +0000

    First "complete" version, still missing some data.

--- tools/pilot/dataimport/koku-dataimport/src/main/java/fi/koku/tools/dataimport/DataImporter.java
+++ tools/pilot/dataimport/koku-dataimport/src/main/java/fi/koku/tools/dataimport/DataImporter.java
@@ -16,7 +16,7 @@ public class DataImporter {
 
   public DataImporter(String[] args) throws Exception {
 
-    Object[] options = new Object[] { "Employee", "Customer" };
+    Object[] options = new Object[] {"Employee", "Customer"};
     int returnvalue = JOptionPane.showOptionDialog(null, "Select file type.", null, JOptionPane.YES_NO_OPTION,
         JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
@@ -33,6 +33,7 @@ public class DataImporter {
       WSCaller caller = new WSCaller();
       File file = chooser.getSelectedFile();
 
+      // User selected Employee
       if (returnvalue == 0) {
         CSVReader reader = new CSVReader(new FileReader(file));
         try {
@@ -40,20 +41,21 @@ public class DataImporter {
         } finally {
           reader.close();
         }
+      // User selected Customer
       } else if (returnvalue == 1) {
         CSVReader reader = new CSVReader(new FileReader(file));
-        try {
-          new CustomerCreator().createCustomers(reader, caller);
-        } finally {
-          reader.close();
-        }
-
-        reader = new CSVReader(new FileReader(file));
         try {
           new LDIFWriter().writeCustomerLDIF(reader, file.getParentFile());
         } finally {
           reader.close();
         }
+
+        reader = new CSVReader(new FileReader(file));        
+        try {
+          new CustomerCreator().createCustomers(reader, caller, file.getParentFile());
+        } finally {
+          reader.close();
+        }               
       }
     }
   }
