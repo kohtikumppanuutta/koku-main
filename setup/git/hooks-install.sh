818b468982721cf9730857235cbdb78dc304e890
marko asplund <marko.asplund@ixonos.com>
2012-05-02 14:18:05 +0300

    KOKU-1287: add hook installation script

---
+++ setup/git/hooks-install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# install KoKu custom Git client-side hooks for each repository
+# 
+# installation:
+# cd $KOKU_REPO_ROOT
+# sh koku-main/setup/git/hooks-install.sh
+#
+
+repos="koku-esb koku-extras koku-main koku-processes koku-service-api koku-services koku-ui"
+hooks="commit-msg msg-check.pl"
+
+for i in $repos; do
+  echo "installing hook to repository: $i"
+  pushd $i/.git/hooks > /dev/null
+
+  # install hooks
+  /bin/echo -n "- hook: "
+  for h in $hooks; do
+    /bin/echo -n "$h "
+    fn="../../../koku-main/setup/git/$h"
+    if [ ! -e  "$fn" ]; then
+      echo "fatal: missing file: $fn"
+      exit 1
+    fi
+    ln -s ../../../koku-main/setup/git/$h .
+  done
+  echo "\n"
+
+  popd > /dev/null
+done
