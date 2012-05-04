193518fb834f1259589f6bbc239c638460f7ff2c
marko asplund <marko.asplund@ixonos.com>
2012-05-04 17:22:03 +0300

    KOKU-1287: remove unnecessary regexp pattern

--- setup/git/msg-check.pl
+++ setup/git/msg-check.pl
@@ -5,9 +5,11 @@ undef $/;
 $msg=<FILE>;
 close(FILE);
 
+# automatic commit messages e.g. merge commits of the form:
+# Merge branch 'develop'
+# bypass service hooks so they don't need to be explicitly allowed.
 if ( $msg =~ /
 	(^KOKU-\d{1,5}:\ [\w\ ]{10,60}$(\n\n\w.*)?(^\#[^\r\n]*$)*)	# normal commit comment
-	|(^Merge\ branch\ '[^']+'$.*)								# branch merge comment
 	/mx ) {
   exit(0);
 }
